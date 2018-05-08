fc3374f36dd6557f8916804ff59ba28e9f6e6bc2
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-08 12:35:01 -0500

    changed to on submit

--- app/public/js/view.js
+++ app/public/js/view.js
@@ -57,7 +57,7 @@ $.get("/api/all", function(data) {
       
   });
 
-$("#add-btn").on("click", function(event) {
+$("#add-btn").on("submit", function(event) {
   event.preventDefault();
 
   // Make a newBook object
