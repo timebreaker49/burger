e0f851af949530507308fef18ab6a48f22debe18
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-08 00:24:30 -0500

    updated a bunch of stuff, hope this works

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var db = require("./models");
 
 // Sets up the Express App
 // =============================================================
@@ -29,6 +30,9 @@ require("./app/routes/html-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+db.sequelize.sync().then(function(){
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+})
+
