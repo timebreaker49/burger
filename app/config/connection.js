77031026a5a028572929219151110f1947b9719f
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-07 22:00:58 -0500

    updated package.json and connection.js

--- app/config/connection.js
+++ app/config/connection.js
@@ -16,5 +16,26 @@ var sequelize = new Sequelize("sequelize_burger", "root", "root", {
   }
 });
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "sequelize_burger"
+});
+};
+
+// Make connection.
+connection.connect(function(err) {
+if (err) {
+  console.error("error connecting: " + err.stack);
+  return;
+}
+console.log("connected as id " + connection.threadId);
+});
+
 // Exports the connection for other files to use
 module.exports = sequelize;
