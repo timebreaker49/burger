626d54d14e9191eeaecf7301b5183cf577b790b9
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-08 00:48:29 -0500

    i have 0 fuxk

--- app/models/burger.js
+++ app/models/burger.js
@@ -4,7 +4,7 @@
 // Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
+// var sequelize = require("../config/connection.js");
 
 // Creates a "Burger" model that matches up with DB
 var Burger = sequelize.define("burger", {
