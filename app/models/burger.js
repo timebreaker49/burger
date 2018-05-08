782c8ac4e86015c551dd0dea2acd71905ac4e873
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-08 01:56:42 -0500

    shits and giggles

--- app/models/burger.js
+++ app/models/burger.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-  var Burger = sequelize.define("Burger", {
+  var Burger = sequelize.define("burger", {
     burger_name: {
       type: DataTypes.STRING,
     },
